ae25eddeddc0ace0afec267bcbc31c9ca4792c33
linux <dani173gonzalez@gmail.com>
2019-06-12 19:14:07 +0200

    first com

---
+++ src/Controller/HomeController.php
@@ -0,0 +1,77 @@
+<?php
+
+namespace App\Controller;
+
+use App\Form\PackType;
+use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+use Symfony\Component\Routing\Annotation\Route;
+use App\Entity\Pack;
+use Symfony\Component\HttpFoundation\Request;
+use App\Entity\Employee;
+use Symfony\Component\Security\Http\Authentication\AuthenticationUtils;
+
+
+
+
+
+class HomeController extends AbstractController
+{
+    /**
+     * @Route("/", name="home")
+     */
+    public function home()
+    {
+        $packs = $this->getDoctrine()->getRepository(Pack::class)->findAll();
+
+
+        return $this->render('home/index.html.twig', [
+            'packs' => $packs,
+        ]);
+    }
+
+    /**
+     * @Route("/home/{id}/show", name="home_show")
+     */
+    public function gomee(Request $request, $id)
+    {
+        $employee = $this->getDoctrine()->getRepository(Employee::class)->findBy(
+            array(
+                'id'=> $id
+            )
+        );
+
+        return $this->render('index/index.html.twig', [
+            'employee' => $employee,
+        ]);
+    }
+
+    /**
+     * @Route("/search", name="home_search")
+     */
+    public function buscador(Request $request)
+    {
+        $pack = new Pack();
+        $form = $this->createForm(PackType::class,$pack);
+        $form->handleRequest($request);
+        if ($form->isSubmitted() && $form->isValid()) {
+           $pack1=$pack->getEmployeeId();
+            $employee= $this->getDoctrine()->getRepository(Employee::class)->findBy(
+                array(
+                    'id'=> $pack1
+                )
+            );
+            return $this->render('home/resultado.html.twig', [
+               'employee'=>$employee
+            ]);
+        }
+
+        return $this->render('home/buscador.html.twig', [
+            'form' => $form->createView(),
+            'pack'=>$pack
+        ]);
+    }
+
+
+
+
+}
--- src/Controller/IndexController.php
+++ src/Controller/IndexController.php
@@ -2,18 +2,29 @@
 
 namespace App\Controller;
 
+use App\Entity\Employee;
+use App\Entity\Pack;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
+use Symfony\Component\HttpFoundation\Request;
+
 
 class IndexController extends AbstractController
 {
     /**
-     * @Route("/index", name="index")
+     * @Route("/index/{id}/show", name="index")
      */
-    public function index()
+    public function index(Request $request, $id)
     {
+        $employee = $this->getDoctrine()->getRepository(Employee::class)->findBy(
+            array(
+                'id'=> $id
+            )
+        );
+
         return $this->render('index/index.html.twig', [
-            'controller_name' => 'IndexController',
+            'employee' => $employee,
         ]);
     }
+
 }
